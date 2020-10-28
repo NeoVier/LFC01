d9ca6437f3ba79bb7308def2b0b531cd05ed4835
Henrique Buss <henrique.buss@hotmail.com>
2020-10-27 22:01:05 -0300

    Documented Types

--- src/Types/Types.elm
+++ src/Types/Types.elm
@@ -1,3 +1,11 @@
+{-
+   Types/Types.elm
+   Author: Henrique da Cunha Buss
+   Creation: October/2020
+   This file contains definitions of the types used for the Elm Architecture
+-}
+
+
 module Types.Types exposing (..)
 
 import File exposing (File)
@@ -8,6 +16,7 @@ import Utils.Utils exposing (filterMaybe)
 
 
 -- MODEL
+-- A model has a current automaton, a history, and a sentence
 
 
 type alias Model =
@@ -15,6 +24,10 @@ type alias Model =
     , currentAutomaton : Result String Models.General
     , currentSentence : String
     }
+
+
+
+-- Initial model - Starts with no history, no automaton and no sentence
 
 
 init : () -> ( Model, Cmd Msg )
@@ -30,6 +43,13 @@ init _ =
 
 
 -- MESSAGE
+-- Messages can be:
+-- xRequested -> the user requested to select a File
+-- xSelected File -> a File was selected to be parsed
+-- xLoaded String -> a String was loaded from a File
+-- ConvertAFNDToAFD -> the user requested to convert the current AFND to a AFD
+-- SetAutomaton Automaton -> the user requested to set the current automaton
+-- SetSentence String -> the user requested to set the current sentence
 
 
 type Msg
